0d5645f3f9a995abe7c6e8ced9e3c6f137d60f7c
Daniels <voiceofwise1@gmail.com>
2022-04-05 19:06:14 +0300

    Attempt at clearing the screen

--- kernel.asm
+++ kernel.asm
@@ -1,5 +1,36 @@
 org 0x8000
 bits 16
+
+; set cursor to top left first
+mov ah, 0x02
+mov bh, 0x00 ; page number
+mov dh, 0x00 ; cursor row
+mov dl, 0x00 ; cursor column
+int 0x10;
+
+; clear screen from top left
+mov al, 0x30 ; clear value
+mov cl, 0x00 ; column
+mov ch, 0x00 ; row
+; assume 80x25 screen
+.row_start:
+    inc ch
+    test ch, 0x19 ; 25
+    jz .clear_end
+    .column_start:
+        inc cl
+        call printCharacter
+        test cl, 0x50 ; 80
+            jz .row_start
+        jmp .column_start
+.clear_end:
+; set cursor back to top left
+mov ah, 0x02
+mov bh, 0x00 ; page number
+mov dh, 0x00 ; cursor row
+mov dl, 0x00 ; cursor column
+int 0x10;
+
 ; write message
 mov si, msg
 call printNullTerminatedString
@@ -8,6 +39,7 @@ jmp $   ; this freezes the system, best for testing
 hlt     ; this makes a real system halt
 ret     ; this makes qemu halt
 
+; AL = character to print
 printCharacter:
     mov bh, 0x00    ; page to write to, page 0 is displayed by default
     mov bl, 0x00    ; color attribute
@@ -15,6 +47,7 @@ printCharacter:
     int 0x10
     ret
 
+; SI = start of a null terminated message
 printNullTerminatedString:
     pusha ; save all register to be able to call this from everywhere
     .loop:
